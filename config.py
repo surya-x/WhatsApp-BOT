6b807a2d5bbc2b979f560d3339a767f6581b2b02
V Surya Kumar <kumarsurya.2001@gmail.com>
2020-04-01 00:11:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # File path of contacts.xlsx
-contacts_filePath   = r"assets\Data\contacts.xlsx"
+contacts_filePath   = r"assets\data\contacts.xlsx"
 
 # File path of parameters.xlsx
-parameters_filePath = r"assets\Data\parameters.xlsx"
+parameters_filePath = r"assets\data\parameters.xlsx"
 
 # File path of gift.png
-imagepath           = r"H:\Phthon\Projects\RAP\assets\Image\gift.png"
+imagepath           = r"H:\Phthon\Projects\RAP\assets\image\gift.png"
 
 # File path of ss.png under screenshots folder
-sspath              = r"H:\Phthon\Projects\RAP\assets\Image\screenshots\ss.png"
+sspath              = r"H:\Phthon\Projects\RAP\assets\image\screenshots\ss.png"
